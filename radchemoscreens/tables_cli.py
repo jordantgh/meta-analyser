ca276858b16e262112615460c438110b9200ac23
Jordan Taylor <jtaylor81284@hotmail.com>
2023-07-02 20:57:01 +0100

    Set edge case handling aside

--- radchemoscreens/tables_cli.py
+++ radchemoscreens/tables_cli.py
@@ -16,128 +16,17 @@ def load_data(file_path):
         raise ValueError("File format not supported.")
     return df
 
-def find_empty_blocks(mask):
-    # Copy the matrix to not modify the original
-    matrix = np.copy(mask)
-
-    # Define the rectangles list
-    rectangles = []
-
-    # Function to create a rectangle
-    def create_rectangle(i, j):
-        # Determine the height of the rectangle
-        height = 0
-        while i + height < len(matrix) and matrix[i + height][j] == 1:
-            height += 1
-
-        # Determine the width of the rectangle
-        width = 0
-        while j + width < len(matrix[0]) and (
-            all(matrix[k][j + width] == 1 for k in range(i, i + height)) and
-            (i + height == len(matrix) or matrix[i + height][j + width] == 0) and
-            (i == 0 or matrix[i - 1][j + width] == 0)
-        ):
-            width += 1
-
-        # Remove the rectangle cells from the matrix
-        for x in range(i, i + height):
-            for y in range(j, j + width):
-                matrix[x][y] = 0
-
-        # Return the rectangle if it's at least 1x2 and connected to an edge
-        if height >= 1 and width >= 2 and (
-            i == 0 or j == 0 or i + height - 1 == len(matrix) - 1 or j + width - 1 == len(matrix[0]) - 1
-        ):
-            return ((i, j), (i + height - 1, j + width - 1))
-        else:
-            return None
-
-    # Loop over every cell in the matrix
-    for j in range(len(matrix[0])):
-        for i in range(len(matrix)):
-            # If the cell is a '1', create a rectangle
-            if matrix[i][j] == 1:
-                rectangle = create_rectangle(i, j)
-                if rectangle is not None:
-                    rectangles.append(rectangle)
-
-    # Return the list of rectangles
-    return rectangles
-
 def parse_tables(df):
     binary_rep = np.array(df.notnull().astype("int"))
-    list_of_dataframes = []
     labeled = label(binary_rep)
 
+    list_of_dataframes = []
     for region in regionprops(labeled):
         minr, minc, maxr, maxc = region.bbox
-        region = df.iloc[minr:maxr, minc:maxc]
-
-        # Binarise the region and invert (so empty cells are `1` and cells with values are `0`)
-        binary_region = np.array(region.isnull().astype("int"))
-
-        # Apply the find_empty_blocks function to get the list of blocks
-        blocks = find_empty_blocks(binary_region)
-        
-        top_blocks = []
-        bottom_blocks = []
-        top_block_columns = []
-
-        # Assign blocks to top_blocks and bottom_blocks and track column ranges for top blocks
-        for block in blocks:
-            if block[0][0] == 0:  # Top block starts from row 0
-                top_blocks.append(block)
-                top_block_columns.append(set(range(block[0][1], block[1][1] + 1)))
-            elif block[1][0] == binary_region.shape[0] - 1:  # Bottom block ends at the last row
-                bottom_blocks.append(block)
-                
-        if not top_blocks:
-            list_of_dataframes.append(region)
+        if maxr - minr <= 1 or maxc - minc <= 1:
             continue
-
-        for block in top_blocks:
-            # Create initial table using top block.
-            top_rows_removed = block[1][0] - block[0][0] + 1  # total rows in top block
-            rowrange = slice(block[1][0] + 1, binary_region.shape[0])
-            colrange = slice(block[0][1], block[1][1] + 1)
-            subregion = region.iloc[rowrange, colrange]
-
-            # Check if a bottom block's column indices match exactly with the subregion's column indices.
-            for bblock in bottom_blocks:
-                bcolrange = slice(bblock[0][1], bblock[1][1] + 1)
-                if colrange == bcolrange:
-                    # If there is an exact match, slice the subregion further to exclude the bottom block.
-                    # Adjust the bottom block slice to be relative to the top block slice.
-                    browrange = bblock[0][0] - top_rows_removed
-                    subregion = subregion.iloc[:browrange, :]
-
-            list_of_dataframes.append(subregion)
-
-        # After processing top blocks, find any column ranges not covered by top blocks and treat these as tables
-        all_columns = set(range(binary_region.shape[1]))
-        covered_columns = set.union(*top_block_columns)
-        uncovered_columns = all_columns - covered_columns
-
-        while uncovered_columns:
-            start_col = min(uncovered_columns)
-            end_col = start_col
-            while end_col + 1 in uncovered_columns:
-                end_col += 1
-
-            uncovered_columns -= set(range(start_col, end_col + 1))
-            colrange = slice(start_col, end_col + 1)
-            subregion = region.iloc[:, colrange]
-
-            # Check if a bottom block's column indices match exactly with the subregion's column indices.
-            for bblock in bottom_blocks:
-                bcolrange = slice(bblock[0][1], bblock[1][1] + 1)
-                if colrange == bcolrange:
-                    # If there is an exact match, slice the subregion further to exclude the bottom block.
-                    # Adjust the bottom block slice to be relative to the top block slice.
-                    browrange = bblock[0][0]
-                    subregion = subregion.iloc[:browrange, :]
-
-            list_of_dataframes.append(subregion)
+        region = df.iloc[minr:maxr, minc:maxc]
+        list_of_dataframes.append(region)
 
     return list_of_dataframes
 
@@ -146,6 +35,13 @@ def main():
     parser.add_argument("file_paths", nargs="+", help="One or more file paths for processing.")
     args = parser.parse_args()
 
+    # Output directory path
+    output_dir = "output_tables"
+
+    # Create output directory if it doesn't exist
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
     # Loop over each specified file
     for file_path in args.file_paths:
         if not os.path.isfile(file_path):
@@ -153,6 +49,9 @@ def main():
             continue
 
         try:
+            # Get parent file name without extension
+            parent_filename = os.path.splitext(os.path.basename(file_path))[0]
+
             data = load_data(file_path)
 
             # Check if the loaded data is a DataFrame or a dictionary of DataFrames (multi-sheet Excel file)
@@ -170,9 +69,10 @@ def main():
             for sheet_name, tables in all_tables.items():
                 for i, df in enumerate(tables):
                   
-                    filename = f"{sheet_name}_df_{i}.csv"  # Generate a unique filename for each DataFrame
-                    df.to_csv(filename, index=False, header=False)
-                    print(f"Saved DataFrame {i} from sheet {sheet_name} as {filename}")
+                    filename = f"{parent_filename}_{sheet_name}_t{i+1}.csv"  # Generate a unique filename for each DataFrame
+                    output_path = os.path.join(output_dir, filename)  # Full output path
+                    df.to_csv(output_path, index=False, header=False)
+                    print(f"Saved DataFrame {i} from sheet {sheet_name} as {output_path}")
 
         except Exception as e:
             print(f"Error processing file {file_path}: {e}")
