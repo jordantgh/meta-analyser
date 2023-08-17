510f638d27da6ac57b900983030806b9dea10e12
Jordan Taylor <jtaylor81284@hotmail.com>
2023-08-17 17:43:18 +0100

    Fix comments

--- radchemoscreens/app/main.py
+++ radchemoscreens/app/main.py
@@ -4,18 +4,13 @@ from view import CRISPRView
 from controller import CRISPRController
 
 def main():
-    # Step 1: Initialize the Qt application
     app = QApplication([])
 
-    # Step 3: Instantiate the model, view, and controller
     model = CRISPRModel()
     view = CRISPRView()
     controller = CRISPRController(model, view)
-    
-    # Display the main window
-    view.show()
 
-    # Step 4: Start the PyQt event loop
+    view.show()
     app.exec_()
 
 if __name__ == "__main__":
