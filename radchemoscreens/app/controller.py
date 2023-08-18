99d7255f8c5dcbe547a09458635cadccd8683a05
Jordan Taylor <jtaylor81284@hotmail.com>
2023-08-18 08:28:45 +0100

    Remove animation init & rename stuff

--- radchemoscreens/app/controller.py
+++ radchemoscreens/app/controller.py
@@ -1,30 +1,27 @@
 import json
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QMessageBox
-from model import SearchThread, FilePreviewThread
 
-class CRISPRController:
+class Controller:
     def __init__(self, model, view):
         self.model = model
         self.view = view
-        self.view.init_load_animation()
         self.connect_sigs()
 
     def connect_sigs(self):
         self.view.search_btn.clicked.connect(self.search_papers)
         self.view.paper_list.itemClicked.connect(self.handle_paper_click)
+        self.view.stop_search_btn.clicked.connect(self.stop_search)
+        self.view.proceed_btn.clicked.connect(self.on_proceed)
 
         self.model.search_thread.article_sig.connect(self.add_paper)
         self.model.search_thread.finished_sig.connect(self.on_search_finished)
         self.model.preview_thread.prev_ready_sig.connect(self.load_preview)
         
-        self.view.stop_search_btn.clicked.connect(self.stop_search)
-        self.view.proceed_btn.clicked.connect(self.on_proceed)
-
     def add_paper(self, article, progress):
         paper_data = self.model.create_paper_data(article)
         self.model.bibliography.add_paper(paper_data)
-        self.view.display_paper_in_ui(paper_data, progress)
+        self.view.display_paper(paper_data, progress)
 
     def search_papers(self):
         if self.model.search_thread.isRunning():
@@ -46,8 +43,8 @@ class CRISPRController:
         if self.model.search_thread.isRunning():
             self.model.search_thread.stop()
             self.model.search_thread.quit()
-            self.model.search_thread.wait()
             self.view.search_status.setText("Stopping search...")
+            self.model.search_thread.wait()
         self.view.prog_bar.hide()
         self.view.search_status.setText("Search stopped.")
         self.view.stop_search_btn.setEnabled(False)
@@ -86,9 +83,9 @@ class CRISPRController:
 
     def determine_data_type_and_display(self, data):
         if isinstance(data, dict):
-            self.view.display_data_in_tabs(data)
+            self.view.display_multisheet_table(data)
         else:
-            self.view.display_data_in_table(data)
+            self.view.display_table(data)
 
     def on_proceed(self):
         selected_data = self.model.extract_selected_papers_and_files()
