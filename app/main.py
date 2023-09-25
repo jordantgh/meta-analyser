2693d885fe7461f7febe0ec14171a43c5e62c696
Jordan Taylor <jtaylor81284@hotmail.com>
2023-09-25 21:49:34 +0100

    Add debug console

--- app/main.py
+++ app/main.py
@@ -2,6 +2,17 @@ from PyQt5.QtWidgets import QApplication
 from model import Model
 from view import View
 from controller import Controller
+from threading import Thread
+
+
+def debug(**kwargs):
+    """Start an interactive REPL with the given local variables."""
+    def start_repl(local_vars):
+        from ptpython.repl import embed
+        embed(globals(), local_vars)
+        
+    repl_thread = Thread(target=start_repl, args=(kwargs,))
+    repl_thread.start()
 
 
 def main():
@@ -9,7 +20,9 @@ def main():
 
     model = Model()
     view = View()
-    _ = Controller(model, view)
+    controller = Controller(model, view)
+
+    debug(model=model, view=view, controller=controller, app=app)
 
     view.show()
     app.exec_()
