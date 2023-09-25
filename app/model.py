e5e5c099335d148f751af5412fcaf3e8c0969811
Jordan Taylor <jtaylor81284@hotmail.com>
2023-09-25 21:48:58 +0100

    Add db support for multiple tables

--- app/model.py
+++ app/model.py
@@ -17,14 +17,22 @@ from sqlalchemy.orm import sessionmaker
 
 import pickle
 
+
 def save_processed_df_to_db(db_manager, table_id, original_file_id, df):
     serialized_df = pickle.dumps(df)
-    db_manager.save_table(table_id, original_file_id, serialized_df)
+    db_manager.save_table(ProcessedTableDBEntry, table_id, original_file_id, serialized_df)
 
 Base = declarative_base()
 
 class ProcessedTableDBEntry(Base):
     __tablename__ = 'processed_tables'
+    
+    table_id = Column(String, primary_key=True)
+    original_file_id = Column(String)
+    table_data = Column(BLOB)
+
+class PostPruningTableDBEntry(Base):
+    __tablename__ = 'post_pruning_tables'
     
     table_id = Column(String, primary_key=True)
     original_file_id = Column(String)
@@ -36,40 +44,46 @@ class TableDBManager:
         Base.metadata.create_all(self.engine)
         self.Session = sessionmaker(bind=self.engine)
 
-    def save_table(self, table_id, original_file_id, table_data):
+    def save_table(self, table_class, table_id, original_file_id, table_data):
         with self.Session() as session:
-            new_table = ProcessedTableDBEntry(
+            new_table = table_class(
               table_id=table_id,
               original_file_id=str(original_file_id),
               table_data=table_data
             )
             session.add(new_table)
             session.commit()
-
-    def update_table(self, table_id, table_data):
+                
+    def update_table(self, table_class, table_id, table_data):
         with self.Session() as session:
-            existing_table = session.query(ProcessedTableDBEntry).filter_by(table_id=table_id).first()
+            existing_table = session.query(table_class).filter_by(table_id=table_id).first()
             if existing_table:
                 existing_table.table_data = table_data
                 session.commit()
 
-    def get_table_data(self, table_id):
+    def get_processed_table_data(self, table_id):
+        return self.get_table_data(ProcessedTableDBEntry, table_id)
+    
+    def get_post_pruning_table_data(self, table_id):
+        return self.get_table_data(PostPruningTableDBEntry, table_id)
+
+    def get_table_data(self, table_class, table_id):
         with self.Session() as session:
-            table = session.query(ProcessedTableDBEntry).filter_by(table_id=table_id).first()
+            table = session.query(table_class).filter_by(table_id=table_id).first()
             if table:
                 table_data = pickle.loads(table.table_data)
                 table_data.reset_index(drop=True, inplace=True)
                 return table_data
             return None
     
-    def get_table_object(self, table_id):
+    def get_table_object(self, table_class, table_id):
         with self.Session() as session:
-            processed_table = session.query(ProcessedTableDBEntry).filter_by(table_id=table_id).first()
-            return processed_table
+            table = session.query(table_class).filter_by(table_id=table_id).first()
+            return table
 
-    def delete_table(self, table_id):  
+    def delete_table(self, table_class, table_id):  
         with self.Session() as session:
-            table = session.query(ProcessedTableDBEntry).filter_by(table_id=table_id).first()
+            table = session.query(table_class).filter_by(table_id=table_id).first()
             if table:
                 session.delete(table)
                 session.commit()
