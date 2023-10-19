bb7424f8e97849f6da29b42df05c4aa921307739
Jordan Taylor <jtaylor81284@hotmail.com>
2023-10-19 22:08:13 +0100

    Add Mode mgmt to constants

--- app/utils/constants.py
+++ app/utils/constants.py
@@ -4,3 +4,9 @@ class PageIdentity(Enum):
     SEARCH = auto()
     PARSED = auto()
     PRUNED = auto()
+
+class Mode(Enum):
+    BROWSING = auto()
+    SEARCHING = auto()
+    PROCESSING = auto()
+    PRUNING = auto()
