8198b71ba6cc04f024be7323f46e0c422122a74e
Jordan Taylor <jtaylor81284@hotmail.com>
2023-10-27 15:24:36 +0100

    Cleanup

--- app/model/file_io.py
+++ app/model/file_io.py
@@ -8,18 +8,16 @@ def extract_dfs(fname: 'str', should_stop: 'bool') -> 'dict[str, pd.DataFrame]':
         return
 
     if fname.endswith(('.xlsx')):
-        # Check should_stop before a potentially long operation
         if should_stop:
             return
-        data = pd.read_excel(fname, sheet_name=None,
-                             header=None, index_col=None)
+        data = pd.read_excel(
+            fname, sheet_name=None, header=None, index_col=None
+        )
     elif fname.endswith('.csv'):
-        # Check should_stop before a potentially long operation
         if should_stop:
             return
         data = {"sheet": pd.read_csv(fname, header=None, index_col=None)}
     else:
-        # Check should_stop before a potentially long operation
         if should_stop:
             return
         data = {"sheet": pd.read_csv(
@@ -45,7 +43,6 @@ def download_supp(url: 'str', should_stop: 'str') -> 'str':
         r.raise_for_status()
         with open(local_fname, 'wb') as f:
             for chunk in r.iter_content(chunk_size=8192):
-                # Check should_stop within the loop
                 if should_stop:
                     os.remove(local_fname)
                     return
