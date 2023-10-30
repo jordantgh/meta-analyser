3f6739eb9dccc019e200230dd5ad756e9b9a4a0b
Jordan Taylor <jtaylor81284@hotmail.com>
2023-10-30 12:36:25 +0000

    Set default config paths

--- app/main.py
+++ app/main.py
@@ -1,38 +1,34 @@
 from PyQt5.QtWidgets import QApplication
-from threading import Thread, Event
-import argparse
-import sys
 import toml
 from datetime import datetime
 import os
+from PyQt5.QtCore import QCoreApplication
 
 from model.model import Model
 from views.view import View
 from controller.controller import Controller
 
-def debug(shutdown_flag, **kwargs):
-    def start_repl(local_vars):
-        from ptpython.repl import embed
-
-        try:
-            while not shutdown_flag.is_set():
-                embed(globals(), local_vars)
-        except SystemExit:
-            pass
-        finally:
-            sys.exit()
-
-    repl_thread = Thread(target=start_repl, args=(kwargs,))
-    repl_thread.start()
 
 def main():
-    parser = argparse.ArgumentParser(description="Run the PyQt5 app.")
-    parser.add_argument('--debug', action='store_true', help='Activate the debug terminal')
-    args = parser.parse_args()
-    
+    app = QApplication([])
+
+    # Define default configuration
+    default_config = {
+        'data': {
+            'path': QCoreApplication.applicationDirPath()
+        }
+    }
+
+    config_path = "config.toml"
+    if not os.path.exists(config_path):
+        # Save the default config to a file
+        with open(config_path, 'w') as f:
+            toml.dump(default_config, f)
+
+    # Load the config file
+    config = toml.load(config_path)
+
     date = datetime.today().strftime('%Y-%m-%d')
-    config = toml.load('config.toml')
-    
     path = os.path.expanduser(config['data']['path'])
     db_path = f"{path}/db/{date}"
     saves_path = f"{path}/saves/{date}"
@@ -40,21 +36,13 @@ def main():
     os.makedirs(db_path, exist_ok=True)
     os.makedirs(saves_path, exist_ok=True)
 
-    app = QApplication([])
-
     model = Model(db_path, saves_path)
     view = View()
-    controller = Controller(model, view)
-
-    shutdown_flag = Event()
-
-    if args.debug:
-        debug(shutdown_flag, model=model, view=view, controller=controller, app=app)
+    _ = Controller(model, view)
 
     view.show()
     app.exec_()
 
-    shutdown_flag.set()
 
 if __name__ == "__main__":
     main()
