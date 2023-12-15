b1e2b19b240d0f96b6171917c6181823d53db894
Jordan Taylor <jtaylor81284@hotmail.com>
2023-12-15 15:39:55 +0000

    Add tags view components

--- app/views/page.py
+++ app/views/page.py
@@ -4,7 +4,7 @@ if TYPE_CHECKING:
     from PyQt5.QtGui import QKeyEvent
 
 from PyQt5.QtCore import Qt, QTimer, QObject, pyqtSignal
-from PyQt5.QtWidgets import QLabel, QProgressBar, QPushButton, QListWidget, QTabWidget, QTextBrowser, QLineEdit, QSizePolicy
+from PyQt5.QtWidgets import QLabel, QProgressBar, QPushButton, QListWidget, QTabWidget, QTextBrowser, QLineEdit, QSizePolicy, QHBoxLayout, QVBoxLayout, QWidget, QListWidgetItem
 
 
 class QPushButton(QPushButton):
@@ -34,6 +34,71 @@ class QListWidget(QListWidget):
             self.itemClicked.emit(self.currentItem())
 
 
+class TagEntryWidget(QLineEdit):
+    tagAdded = pyqtSignal(str)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setPlaceholderText("Type a descriptive tag and press Enter...")
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Return or event.key() == Qt.Key_Enter:
+            text = self.text().strip().replace(" ", "_")
+            if text:
+                self.tagAdded.emit(text)
+                self.clear()
+        super().keyPressEvent(event)
+
+
+class TagWidget(QWidget):
+    # Custom widget that includes a label and a delete button
+    removeTag = pyqtSignal(str)
+
+    def __init__(self, tag, parent=None):
+        super().__init__(parent)
+        self.tag = tag
+
+        # Layout
+        layout = QHBoxLayout(self)
+        tag_label = QLabel(f"#{tag}")
+        remove_button = QPushButton("X")
+        remove_button.setFixedSize(20, 20)  # Small, fixed-size button
+        layout.addWidget(tag_label)
+        layout.addWidget(remove_button)
+        layout.addStretch()
+        layout.setContentsMargins(0, 0, 0, 0)
+
+        # Connect the remove button signal
+        remove_button.clicked.connect(self.emitRemoveSignal)
+
+    def emitRemoveSignal(self):
+        self.removeTag.emit(self.tag)
+
+class TagsDisplayWidget(QListWidget):
+    tagRemoved = pyqtSignal(str)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setSelectionMode(QListWidget.NoSelection)
+
+    def addTag(self, tag):
+        item = QListWidgetItem(self)
+        tag_widget = TagWidget(tag, self)
+        item.setSizeHint(tag_widget.sizeHint())
+        self.addItem(item)
+        self.setItemWidget(item, tag_widget)
+        tag_widget.removeTag.connect(self.removeTag)
+
+    def removeTag(self, tag):
+        # Find the item and remove it from the list
+        for index in range(self.count()):
+            item = self.item(index)
+            widget = self.itemWidget(item)
+            if widget.tag == tag:
+                self.takeItem(index)
+                break
+        self.tagRemoved.emit(tag)
+
 class PageElements(QObject):  # QObject needed for signalling
     def __init__(self, parent_tab: 'TabPage'):
         super().__init__()
@@ -62,7 +127,23 @@ class PageElements(QObject):  # QObject needed for signalling
 
         self.metadata_view = QTextBrowser()
         self.metadata_view.setOpenExternalLinks(True)
-        self.outer_tab_widget.addTab(self.metadata_view, "Metadata")
+
+        self.tags_entry_widget = TagEntryWidget(parent_tab)
+        self.tags_display_widget = TagsDisplayWidget(parent_tab)
+        self.tags_layout = QHBoxLayout()
+        self.tags_layout.addWidget(self.tags_entry_widget)
+        self.tags_display_layout = QVBoxLayout()
+        self.tags_display_layout.addWidget(self.tags_display_widget)
+
+        self.tags_widget = QWidget()
+        self.tags_full_layout = QVBoxLayout(self.tags_widget)
+        self.tags_full_layout.addLayout(self.tags_layout)
+        self.tags_full_layout.addLayout(self.tags_display_layout)
+
+        self.info_tabs_widget = QTabWidget(parent_tab)
+        self.info_tabs_widget.addTab(self.data_previews, "Previews")
+        self.info_tabs_widget.addTab(self.metadata_view, "Metadata")
+        self.info_tabs_widget.addTab(self.tags_widget, "Tags")
 
         self.loading_label = QLabel(parent_tab)
         self.loading_label.setAlignment(Qt.AlignCenter)
